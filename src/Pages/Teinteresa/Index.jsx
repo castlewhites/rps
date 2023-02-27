cb3f5063402c3258b10d7f9bd2a357a401a366cc
sebastian castiblanco <scastiblanco2003@gmail.com>
2023-02-27 17:17:03 -0500

    ahorcado listo, falta logica para voltear la carta

--- src/Pages/Teinteresa/Index.jsx
+++ src/Pages/Teinteresa/Index.jsx
@@ -12,10 +12,10 @@ let tiempo = false;
 
 
 function TeInteresa() {
-  const palabras = ["hola", "mundo", "react", "javascript"];
+  const palabras = ["hermosa", "princesa", "mia", "brillante"];
 
 
-  const [palabra, setPalabra] = useState(palabras[Math.floor(Math.random() * palabras.length)]);
+  const [palabra, setPalabra] = useState(palabras[Math.floor(Math.random() * palabras.length)].toUpperCase());
   const [intentos, setIntentos] = useState(6);
   const [letrasAdivinadas, setLetrasAdivinadas] = useState([]);
 
@@ -27,11 +27,11 @@ function TeInteresa() {
       }
     }
   };
-  const Reset =() => {
+  const Reset = () => {
     window.location.reload()
 
   }
-
+  console.log();
 
   return (
     <>
@@ -42,34 +42,39 @@ function TeInteresa() {
             <h1 className="memory-title">¿LISTA PARA DESCUBRIRLO?</h1>
           </div>
           <div className="cards-container">
-            <h1>Ahorcado</h1>
-            <div>
-              {palabra.split("").map((letra, index) => (
-                <span key={index}>
-                  {letrasAdivinadas.includes(letra) ? letra : "___ "}
-                </span>
-              ))}
-            </div>
-            <div>
-              {intentos > 0 ? (
-                "abcdefghijklmnopqrstuvwxyz".split("").map((letra, index) => (
-                  <button key={index} onClick={() => manejarIntento(letra)}>
-                    {letra}
-                  </button>
-                ))
-              ) : (
-                <>                
-                  <p>Has perdido. La palabra era: {palabra}</p>
-                  <button onClick={Reset} >Reset</button>
-                </>
+            <div className="">
+              <div className="boxGuideA">
+                <span>AHORCADO</span>
+              </div>
+              <div className="container-word">
+                {palabra.toUpperCase().split("").map((letra, index) => (
+                  <span className="letters" key={index}>
+                    {letrasAdivinadas.includes(letra) ? letra : <span className="line-letter">__ </span> }
+                  </span>
+                ))}
+              </div>
+              <div className="container-hanged-btn">
+                {intentos > 0 ? (
+                  "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("").map((letra, index) => (
+                    <button className="hanged-letters" key={index} onClick={() => manejarIntento(letra)}>
+                      {letra}
+                    </button>
+                  ))
+                ) : (
+                  <>
+                    <h2>¡HAS PERDIDO!</h2>
+                    <p className="note">LA PALABRA ERA: <span>{palabra}</span></p>
+                    <button className="reset-btn" onClick={Reset} >Reset</button>
+                  </>
 
-              )}
+                )}
+              </div>
             </div>
 
 
             <div className="informacion">
               <h2 id="aciertos" className="estadisticas">Intentos restantes: {intentos}</h2>
-              <h2 id="movimientos" className="estadisticas">Letras adivinadas: {letrasAdivinadas.join(", ")}</h2>
+              <h2 id="movimientos" className="estadisticas">Letras usadas: {letrasAdivinadas.join(", ")}</h2>
             </div>
           </div>
         </div>
